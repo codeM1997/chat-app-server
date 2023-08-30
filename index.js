477e707cfce410a08daf2c4466a7705c12095455
anshul <anshul_mittal12@hotmail.com>
2023-08-31 00:28:00 +0530

    add: backend code

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { Server } = require('socket.io');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+const server = app.listen('3001', () => {
+    console.log('Server running on port 3001....')
+})
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on('connection', (socket) => {
+    socket.emit('welcome', 'Welcome to the chat room we')
+    socket.on('join_room', (data) => {
+        socket.join(data)
+    })
+    socket.on('send_message', (data) => {
+        socket.to(data.room).emit("receive_message", data.content);
+    })
+    socket.on('disconnect', () => {
+        console.log('server disconnected')
+    })
+})
